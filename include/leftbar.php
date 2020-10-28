5ce187409894b2c3ddc9b2c10717babbbfa16db8
折老爷 <60703460+Dark-flame-master1@users.noreply.github.com>
2020-10-28 20:52:49 +0800

    Add files via upload

---
+++ include/leftbar.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="stylesheet" href="https://cdn.staticfile.org/twitter-bootstrap/3.3.7/css/bootstrap.min.css">  
+    <script src="https://cdn.staticfile.org/jquery/2.1.1/jquery.min.js"></script>
+    <script src="https://cdn.staticfile.org/twitter-bootstrap/3.3.7/js/bootstrap.min.js"></script>
+    
+    <link rel="stylesheet" href="home.css">
+</head>
+<body>
+    
+        <a href="home.php"><img class='logo img-responsive' src="img/logo.png" alt="logo"></a>
+        <div class="row">
+                <div class="col-md-3">
+                    <ul class="list-unstyled leftbar nav">   
+                        <a href="javascript:volid(0);"><span><!-- <img src="" alt="" width='40px' height='40px'>--> 登录/注册</span></a> 
+                        <a href="search.php"><span class="glyphicon glyphicon-globe"></span> Search/搜索</a>
+                        <a href="major.php"><span class="glyphicon glyphicon-eye-open"></span> Major/大学专业</a>
+                        <a href="ranking.php"><span class="glyphicon glyphicon-edit"></span> Ranking/综合排名</a>
+                        <a href="javascript:volid(0);"><span class="glyphicon glyphicon-user"></span> SchoolMate/校友</a>
+                        <a href="javascript:volid(0);"><span class="glyphicon glyphicon-pencil"></span> 留学问答</a>
+                        <a href="about.php"><span class="glyphicon glyphicon-question-sign"></span> About/关于</a>
+                        <img class='cover'src="img/lock.svg" alt="">
+                        <img class='cover1'src="img/lock.svg" alt="">
+                        <img class='cover2'src="img/lock.svg" alt="">
+                    </ul>
+                    <br>
+                    <br>
+                </div>
+</body>
+</html>
---
+++ include/rightbar.php
@@ -0,0 +1,63 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="stylesheet" href="https://cdn.staticfile.org/twitter-bootstrap/3.3.7/css/bootstrap.min.css">  
+    <script src="https://cdn.staticfile.org/jquery/2.1.1/jquery.min.js"></script>
+    <script src="https://cdn.staticfile.org/twitter-bootstrap/3.3.7/js/bootstrap.min.js"></script>
+    <title>寻找心仪大学吧 - WillCloudy</title>
+    <link rel="stylesheet" href="home.css">
+</head>
+<body>
+        <div class="col-md-3">
+            <div class='tuijian'>
+                <div class='ulist'>
+                    <h4 style='font-weight:bold;text-align:center;margin-top:20px;'>对比国内外物价</h4>
+                        <div class="row">
+                            <div class='col-md-11'>
+                                <ul style='list-style:none;padding-left:20px;padding-top:2px'>
+                                    <li>
+                                        <div class='toprank'>
+                                            <div >
+                                                <a href='country.php?country=UK'><img src='img/enland.jpg' alt='' class='img-responsive'></a>
+                                            </div>
+                                            <a href="country.php?country=UK"><h4 style='text-align:center'>英国/Prices in British</h4></a>
+                                        </div>
+                                    </li>
+                                    <li>
+                                        <div class='toprank'>
+                                            <div >
+                                                <a href='country.php?country=CAN'><img src='img/caland.jpg' alt='' class='img-responsive'></a>
+                                            </div>
+                                            <a href="country.php?country=CAN"><h4 style='text-align:center'>加拿大/Prices in Canada</h4></a>
+                                        </div>
+                                    </li>
+                                    <li>
+                                        <div class='toprank'>
+                                            <div >
+                                                <a href='search.php?country=AUS'><img src='img/auland.jpg' alt='' class='img-responsive'></a>
+                                            </div>
+                                            <a href="search.php?country=AUS"><h4 style='text-align:center'>澳大利亚/Prices in Australia</h4></a>
+                                        </div>
+                                    </li>
+                                </ul>
+                            </div>
+                            
+                        </div>
+                        </div>
+                </div>
+                <br>
+            </div>
+
+            <!-- <div class="col-md-3">
+                <div class='tuijian'>
+                    <div class="plist">
+                        <h3 style='font-weight:bold;'>Who to follow</h3>
+                    </div>
+                </div>
+            </div> -->
+
+        </div>
+</body>
+</html>
